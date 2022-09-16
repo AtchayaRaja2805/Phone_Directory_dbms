e02e6a0400728a51aa512e23247f7d8675263c1c
AtchayaRaja2805 <113582568+AtchayaRaja2805@users.noreply.github.com>
2022-09-16 14:34:18 +0530

    Update app use.sql

--- app use.sql
+++ app use.sql
@@ -18,8 +18,10 @@ country varchar(30) not null,
 pincode int,
 country_code int,
 nationality varchar(20) not null,
-nri_resident varchar(8) not null
+nri_resident varchar(9) not null
 );
 insert into credentials(first_name,last_name,department,employee_id,primary_number,secondary_number,official_mailid,secondary_mailid,door_no,street_name,
-city,state,country,pincode,country_code,nationality,nri_resident)values("atchaya","raja","ai",09,8667230378,6382353773,"727721euai009@skcet.ac.in","atchayaraja28@gmail.com",19,"manickam nagar","pudukkottai",
-"tamil nadu","india",622003,91,"indian","resident");
+city,state,country,pincode,country_code,nationality,nri_resident)values("atchaya","raja","ai",2,8667230378,6382353773,"727721euai009@skcet.ac.in","atchayaraja28@gmail.com",19,"manickam nagar","pudukkottai",
+"tamil nadu","india",622003,91,"indian","resident"),("bala","murugan","ai",11,9363423876,9715054973,"727721eyai011@skcet.ac.in","balamanickam118@gmail.com",80,"east street",
+"punruti","tamil nadu","india",607103,91,"indian","residient"),("kavya","prakash","ai",27,7894561230,0321654987,"727721euai027@skcet.ac.in","kavyaprakash27@gmail.com",27,"main street","mettupalayam",
+"tamil nadu","india",654321,91,"indian","resident"); 
